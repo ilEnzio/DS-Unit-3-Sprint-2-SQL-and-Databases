d259be69c42247a7bea7e2c1af7307e0cb4cc035
ilenzio <westfury@gmail.com>
2020-09-09 13:39:51 -0400

    Titanic Insert working

--- module2-sql-for-analysis/titan_insert.py
+++ module2-sql-for-analysis/titan_insert.py
@@ -3,6 +3,8 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 import psycopg2
+from psycopg2.extras import execute_values
+
 
 import pandas as pd
 
@@ -32,21 +34,44 @@ titan_df = pd.read_csv(
 
 # this basically takes all the data and pairs the columns
 # with the values as tuples
+# but this might be wrong format for tuple list
 data_tuple_list = []
-for x in range(0, len(titan_df.columns)):
-    temp_tup = (titan_df.columns[x], list(titan_df.iloc[:, x].values))
-    data_tuple_list.append(temp_tup)
+# for x in range(0, len(titan_df.columns)):
+#     temp_tup = (titan_df.columns[x], list(titan_df.iloc[:, x].values))
+#     data_tuple_list.append(temp_tup)
+
+#   tuple list vers 2!
+
+# records = titan_df.to_records(index=False)
+# data_tuple_list = list(records)
+
+# tuple list vers 3
+
+data_tuple_list = [list(row) for row in titan_df.itertuples(index=False)]
 
 # Now I need to learn the query
 
 # Create the table with cursor
-CREATE_TABLE = """CREATE TABLE titanic_passengers(
-  Survived int, Pclass int, Name varchar(100),
-  Sex varchar(3), Age float, Siblings_Spouses_Aboard int,
-  Parents_Children_Aboard int, Fare float);
-"""
+CREATE_TABLE = """ 
+    DROP TABLE IF EXISTS titanic_passengers;
+                    CREATE TABLE titanic_passengers (
+        Survived int,
+        Pclass int,
+        Name varchar(100),
+        Sex varchar(100),
+        Age float,
+        Siblings_Spouses_Aboard int,
+        Parents_Children_Aboard int,
+        Fare float
+    );"""
+
 
 cursor.execute(CREATE_TABLE)
+
+
+insertion_query = "INSERT INTO titanic_passengers (Survived, Pclass, Name,  Sex, Age, Siblings_Spouses_Aboard, Parents_Children_Aboard, Fare) VALUES %s"
+
+execute_values(cursor, insertion_query, data_tuple_list)
 
 connection.commit()
 
